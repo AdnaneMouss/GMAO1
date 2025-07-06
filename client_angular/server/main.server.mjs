83c873a12527068038dc7e85fdc32da26fd7d77c
Adnane <belbsiramira@gmail.com>
2025-07-06 04:05:31 +0100

    Auto-generated commit

---
+++ client_angular/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{e as a}from"./chunk-DYVVC5W2.mjs";import"./chunk-6ASZ7FCT.mjs";import"./chunk-AUXKI76G.mjs";import"./chunk-RGNWAOKM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
